b024d3cfabac8bdaa9ad6252bf5955eea95cc2dd
lossycrypt <lossycrypt@users.noreply.github.com>
2021-07-13 15:24:19 +0200

    Prototype + Prototype.get_absolute_order

--- erlib/factorio/Prototype.lua
+++ erlib/factorio/Prototype.lua
@@ -20,6 +20,9 @@ local say,warn,err,elreq,flag,ercfg=table.unpack(require(elroot..'erlib/shared')
 -- (Factorio does not allow runtime require!)                                 --
 -- -------------------------------------------------------------------------- --
 
+local String       = elreq('erlib/lua/String'     )()
+
+
 -- -------------------------------------------------------------------------- --
 -- Module                                                                     --
 -- -------------------------------------------------------------------------- --
@@ -37,41 +40,37 @@ local Prototype,_Prototype,_uLocale = {},{},{}
 --------------------------------------------------------------------------------
 
 ----------
--- Merges type, group, subgroup, order and name.
--- Allows sorting arrays or arbitary prototype combinations.
+-- Merges object_name, group, subgroup, order and name.
+-- Used for sorting arrays or arbitary prototype combinations.
+--
+-- __Memoized__: Reads each prototype only once per session. Subsequent
+-- access is native lua table lookup speed.
 --
 -- @tparam string object_name The runtime-type of the desired prototype.
--- Valid inputs formats are:
+-- Valid input formats are:
 -- `"LuaVirtualSignalPrototype"`, 
 -- `"LuaVirtualSignal"`,
 -- `"VirtualSignal"` or
--- `"virtual_signal"`
+-- `"virtual_signal"`. Try to stick to one format as __mixing them
+--   costs additional memory__ for memoization.
 -- @tparam string name
 -- @treturn string
 -- @function Prototype.get_absolute_order
 do
-
-  -- "CamelCase" -> "camel_case"
-  local function uncamel(str)
-    return str:gsub('(%a)(%u)','%1_%2'):lower()
-    end
-
   local function _get_order(object_name, name)
     local category = String.to_snake_case(object_name)
       :gsub('^lua_?',''):gsub('_?prototypes?$','')
     local prot = game[category..'_prototypes'][name]
     local has_group = (pcall(function() return prot.group    end))
-    -- local has_type     = (pcall(function() return prot.type     end))
     return table.concat{
       category,
-      -- has_type  and prot.type or '',
       has_group and prot.group.order    or '',
       has_group and prot.subgroup.order or '',
       prot.order,
       prot.name
       }
     end
-
+  --
   local order = setmetatable({}, {__index = function(self, object_name)
     self[object_name] = setmetatable({}, {__index = function(self, name)
       self[name] = _get_order(object_name, name)
@@ -80,35 +79,11 @@ do
       end})
     return self[object_name]
     end})
-  
-  
+  --
 function Prototype.get_absolute_order(object_name, name)
   return order[object_name][name]
   end
   end
-
--- get_real_order
-
-
--- From babelfish
--- local get_full_prototype_order; do
-  -- local has_group = setmetatable({},{__index=function(self, object_name)
-    -- local category = object_name:match('Lua(.+)Prototype')
-      -- :gsub('%u','_%1'):sub(2):lower() -- "VirutalSignal" -> "virtual_signal"
-    -- for _, prot in pairs(game[category..'_prototypes']) do
-      -- self[object_name] = (pcall(function() return prot.group end))
-      -- break end
-    -- return self[object_name] end})
-  -- function get_full_prototype_order(prot)
-    -- return table.concat{
-      -- has_group[prot.object_name] and prot.group.order    or '',
-      -- has_group[prot.object_name] and prot.subgroup.order or '',
-      -- prot.order,
-      -- prot.name
-      -- }
-    -- end
-  -- end
-
 
 
 -- -------------------------------------------------------------------------- --
